1c9f0aeb6b6e3de9869470b1da0fe043ca934b87
siddhant <siddhantawathare6@gmail.com>
2026-04-13 12:11:44 +0530

    following google-jib image standard removed dockerFile, 3 approaches to read values

--- src/main/java/com/bank/account/AccountServiceApplication.java
+++ src/main/java/com/bank/account/AccountServiceApplication.java
@@ -1,14 +1,17 @@
 package com.bank.account;
 
+import com.bank.account.dto.AccountContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = AccountContactInfoDto.class)
 @OpenAPIDefinition(
         info = @Info(
                 title = "Account ms REST api docs",
